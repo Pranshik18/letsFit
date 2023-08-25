0357b1076f98a334afb890282b162ec46143cc09
Pranshik18 <pranshiksharma3453@gmail.com>
2023-08-26 00:55:35 +0530

    Add: Splash Screen

--- App.tsx
+++ App.tsx
@@ -5,12 +5,14 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppNavigator from './app/navigators/AppNavigator';
-
+import SplashScreen from 'react-native-splash-screen';
 
 function App(): JSX.Element {
-
+  useEffect(()=>{
+    SplashScreen.hide();
+  },[])
   return (
     <AppNavigator />
   );
